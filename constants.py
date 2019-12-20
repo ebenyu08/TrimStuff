104d9f2011cf4cb0c9e7f80ccf6856956481c6be
Benjamin <egri.benyu@gmail.com>
2019-12-20 17:15:01 +0100

    user settings, file size estimation

--- constants.py
+++ constants.py
@@ -1,15 +1,20 @@
 # TITLE
 APP_TITLE = "Trim Stuff"
+SETTINGS_TITLE = "Settings"
+HELP_TITLE = "Help"
 
 # ENTRIES
 FILE_PATH_ENTRY = "File"
-CUT_FROM_END_ENTRY = "Cut From End (sec)"
+DURATION_ENTRY = "Duration (sec)"
 START_FROM_ENTRY = "Start From (sec)"
 
 # BUTTONS
 OUTPUT_BUTTON = "Output"
 BROWSE_BUTTON = "Browse"
 TRIM_BUTTON = "Trim"
+OUTPUT_CHANGE_BUTTON = "Change Output"
+INPUT_CHANGE_BUTTON = "Change Input"
+SAVE_SETTINGS_BUTTON = "Save Settings"
 
 # OPTION BOXES
 RESOLUTION_OPTION_BOX = "Resolution"
@@ -20,14 +25,14 @@ CRF_SPIN_BOX = "CRF"
 LOG_MESSAGE = "Log"
 
 # FRAMES
-OPTIONS_FRAME = "Options"
+PARAMETERS_FRAME = "Parameters"
 LOG_FRAME = LOG_MESSAGE
 
 # OPTIONS
 RES_720 = "1280x720"
 RES_1080 = "1920x1080"
 SCALING_OPTIONS = [RES_720, RES_1080]
-CRF_OPTIONS = range(25, -1, -1)
+CRF_OPTIONS = list(range(25, -1, -1))
 PRESET_OPTIONS = [
     "ultrafast",
     "superfast",
@@ -42,5 +47,9 @@ PRESET_OPTIONS = [
 ]
 
 # STRINGS
+FFMPEG_LINK = "Download ffmpeg here..."
+INPUT_PATH_LABEL = "Browse folder"
+OUTPUT_PATH_LABEL = "Output Folder"
+SIZE_LABEL = "Size(Nagyon Beta Version :D): "
 SAVE_AS = "Save as..."
 DEFAULT_OUTPUT = "D:/Movies/Recordings/Trimmed Recordings/out.mp4"
--- gui.py
+++ gui.py
@@ -1,7 +1,12 @@
 import subprocess
-from pathlib import Path
+import json
+import webbrowser
+from pymediainfo import MediaInfo
+from threading import Thread
 from appJar import gui
 from constants import *
+
+user_settings = json.load(open("settings/config.json"))
 
 file_path = DEFAULT_OUTPUT
 
@@ -31,69 +36,131 @@ def save_as():
 
 
 def estimate_file_size():
-    selected_file = Path(app.getEntry(FILE_PATH_ENTRY))
-    info = selected_file.stat()
+    video_info = MediaInfo.parse(app.getEntry(FILE_PATH_ENTRY))
 
     multiplier = 1
+    size = video_info.tracks[1].stream_size
+    original_dur = int(video_info.tracks[1].mdhd_duration) / 1000
+    changed_start = app.getEntry(START_FROM_ENTRY)
+    changed_dur = app.getEntry(DURATION_ENTRY)
+    if not changed_dur:
+        if not changed_start:
+            changed_dur = original_dur
+        else:
+            changed_dur = original_dur - int(changed_start)
+
     res = app.getOptionBox(RESOLUTION_OPTION_BOX)
     crf = app.getSpinBox(CRF_SPIN_BOX)
     preset = app.getOptionBox(PRESET_OPTION_BOX)
+    preset_index = PRESET_OPTIONS.index(preset)
 
     if res == RES_720:
         multiplier *= 0.667
 
-    multiplier *= (100 - float(crf) * 3.25) / 100
+    multiplier *= (100 - float(crf) * (0.8 + (int(crf)/10))) / 67
+    multiplier *= 20 / (preset_index + 20)
+    multiplier *= float(changed_dur) / original_dur
 
-    app.setLabel("Size(estimated): ", "Size(estimated): " + str(info.st_size / 1000 / 1000 * multiplier) + " MB")
+    app.setLabel(SIZE_LABEL, SIZE_LABEL +
+                 str(int(size / 1000 / 1000 * multiplier)) + " MB")
+
+
+def trim_video_threaded():
+    thread = Thread(target=trim_video)
+    thread.start()
 
 
 def trim_video():
     # get parameters
     selected_file = app.getEntry(FILE_PATH_ENTRY)
 
-    resolution = app.getOptionBox(RESOLUTION_OPTION_BOX)
-
     start_from = app.getEntry(START_FROM_ENTRY)
     if not start_from:
         start_from = "0"
 
-    cut_from = app.getEntry(CUT_FROM_END_ENTRY)
-    if not cut_from:
-        cut_from = "0"
+    duration = app.getEntry(DURATION_ENTRY)
+    if not duration:
+        duration = "0"
 
+    resolution = app.getOptionBox(RESOLUTION_OPTION_BOX)
     crf = app.getSpinBox(CRF_SPIN_BOX)
-
     preset = app.getOptionBox(PRESET_OPTION_BOX)
-
-    print(selected_file)
-    print(resolution)
 
     command = "ffmpeg -y " \
               "-ss " + start_from + \
-              " -t " + cut_from + \
+              " -t " + duration + \
               " -i \"" + selected_file + \
               "\" -c:v libx264 -crf " + crf + \
               " -preset " + preset + \
               " -vf scale=" + resolution + \
               " \"" + file_path + "\""
-    print(command)
+
+    app.setMessage(LOG_MESSAGE, "Trimming started... \n\n")
     popen = subprocess.Popen(command, stdout=subprocess.PIPE, universal_newlines=True)
-
-    for line in popen.stdout:
-        app.setMessage(LOG_MESSAGE, line.decode())
-    popen.stdout.close()
-
     exit_code = popen.wait()
+    app.setMessage(LOG_MESSAGE, app.getMessage(LOG_MESSAGE) + "Trimming finished with exit code: " + str(exit_code))
     if exit_code:
         app.errorBox("Error", "There was an error while trimming...")
 
 
+def open_options():
+    app.showSubWindow(SETTINGS_TITLE)
+
+
+def close_options():
+    app.hideSubWindow(SETTINGS_TITLE)
+
+
+def change_default_folder(button_name):
+    path = app.directoryBox()
+
+    if button_name == OUTPUT_CHANGE_BUTTON:
+        user_settings["output_folder"] = path
+        app.setEntry(OUTPUT_PATH_LABEL, path)
+    elif button_name == INPUT_CHANGE_BUTTON:
+        user_settings["input_folder"] = path
+        app.setEntry(INPUT_PATH_LABEL, path)
+    open_options()
+
+
+def save_settings():
+    if json.dump(user_settings, open("settings/config.json", "w")):
+        close_options()
+
+
+def open_ffmpeg_page():
+    webbrowser.open("https://ffmpeg.zeranoe.com/builds/")
+
+
 with gui(APP_TITLE) as app:
-    with app.labelFrame(OPTIONS_FRAME):
-        app.setSize(640, 480)
+    with app.labelFrame(PARAMETERS_FRAME):
+        app.setSize(400, 600)
+        app.setResizable(False)
         app.setTitle(APP_TITLE)
         app.icon_path = "resources/icons8-color-48.png"
 
+        # User Settings
+        app.addMenuItem(SETTINGS_TITLE, SETTINGS_TITLE, open_options)
+        app.addMenuList(HELP_TITLE, [FFMPEG_LINK], funcs=open_ffmpeg_page)
+        app.startSubWindow(SETTINGS_TITLE)
+
+        app.addLabel(OUTPUT_PATH_LABEL)
+        app.addEntry(OUTPUT_PATH_LABEL, row="p", column=1)
+        app.setEntry(OUTPUT_PATH_LABEL, user_settings["output_folder"])
+        app.addButton(OUTPUT_CHANGE_BUTTON, change_default_folder, row="p", column=2)
+
+        app.setSticky("we")
+        app.addLabel(INPUT_PATH_LABEL)
+        app.addEntry(INPUT_PATH_LABEL, row="p", column=1)
+        app.setEntry(INPUT_PATH_LABEL, user_settings["input_folder"])
+        app.addButton(INPUT_CHANGE_BUTTON, change_default_folder, row="p", column=2)
+
+        app.setSticky("")
+        app.addButton(SAVE_SETTINGS_BUTTON, save_settings, colspan=3)
+
+        app.stopSubWindow()
+
+        # Input File
         app.setExpand("NONE")
         app.setStretch("")
         app.setInPadding([5, 5])
@@ -108,16 +175,20 @@ with gui(APP_TITLE) as app:
 
         app.setExpand("ALL")
         app.setStretch("none")
-        app.setSticky("w")
+        app.setSticky("ws")
 
+        # Quality Settings
         app.addLabel(RESOLUTION_OPTION_BOX)
         app.addOptionBox(RESOLUTION_OPTION_BOX, SCALING_OPTIONS, row="p", column=1)
         app.setOptionBoxChangeFunction(RESOLUTION_OPTION_BOX, estimate_file_size)
 
+        app.setSticky("w")
         app.addLabel(PRESET_OPTION_BOX)
         app.addOptionBox(PRESET_OPTION_BOX, PRESET_OPTIONS, row="p", column=1)
         app.setOptionBox(PRESET_OPTION_BOX, PRESET_OPTIONS.index("veryfast"))
+        app.setOptionBoxChangeFunction(PRESET_OPTION_BOX, estimate_file_size)
 
+        app.setSticky("wn")
         app.addLabel(CRF_SPIN_BOX)
         app.addSpinBox(CRF_SPIN_BOX, CRF_OPTIONS, row="p", column=1)
         app.setSpinBox(CRF_SPIN_BOX, 18)
@@ -127,23 +198,34 @@ with gui(APP_TITLE) as app:
         app.setStretch("both")
         app.setSticky("ws")
 
+        # Trimming Settings
         app.addLabel(START_FROM_ENTRY)
         app.addEntry(START_FROM_ENTRY, row="p", column=1)
         app.setEntryWidth(START_FROM_ENTRY, 5)
+        app.setEntryChangeFunction(START_FROM_ENTRY, estimate_file_size)
 
         app.setSticky("wn")
-        app.addLabel(CUT_FROM_END_ENTRY)
-        app.addEntry(CUT_FROM_END_ENTRY, row="p", column=1)
-        app.setEntryWidth(CUT_FROM_END_ENTRY, 5)
+        app.addLabel(DURATION_ENTRY)
+        app.addEntry(DURATION_ENTRY, row="p", column=1)
+        app.setEntryWidth(DURATION_ENTRY, 5)
+        app.setEntryChangeFunction(DURATION_ENTRY, estimate_file_size)
 
-        app.setSticky("")
+        app.setPadding([0, 10])
+        app.setSticky("es")
         app.addNamedButton(SAVE_AS, OUTPUT_BUTTON, save_as)
-        app.addButton(TRIM_BUTTON, trim_video, row="p", column=1)
+        app.setSticky("s")
+        app.addButton(TRIM_BUTTON, trim_video_threaded, row="p", column=1)
         app.setButtonBg(TRIM_BUTTON, "lightgreen")
 
-        app.addLabel("Size(estimated): ", colspan=2)
+        app.setExpand("NONE")
+        app.setPadding([0, 0])
+        app.setSticky("w")
+        app.addLabel(SIZE_LABEL, colspan=3)
+        app.setExpand("ALL")
 
-    with app.labelFrame(LOG_FRAME, row="p", column=2):
+    with app.labelFrame(LOG_FRAME):
+        app.startScrollPane(LOG_FRAME)
         app.addEmptyMessage(LOG_MESSAGE)
+        app.stopScrollPane()
 
 app.go()
